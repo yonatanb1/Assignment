a595cf7519b41a9dd80db6556ad1ad7c8a6b1ab5
yonatanb1 <107631277+yonatanb1@users.noreply.github.com>
2022-06-30 00:45:22 +0300

    lab.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Program
+{
+    class prog
+    {
+        static void Main(String[] args)
+        {
+            int number;
+             
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("please enter a number:");
+                number = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("please enter a number:");
+                number = Int32.Parse(Console.ReadLine());
+                if (number >i)
+            {
+                Console.WriteLine(number +" is greater " );
+            }else if (number <i)
+            {
+                Console.WriteLine(number + " is lesser  ");
+            }
+            else
+            {
+                    Console.WriteLine(number + "is equal  "  );
+                        }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
