4c1ca0125bec635d0e5d5f79482ca5293e4736ee
yonatanb1 <107631277+yonatanb1@users.noreply.github.com>
2022-07-04 17:54:42 +0300

    Assignment 3

--- Program.cs
+++ Program.cs
@@ -1,33 +1,58 @@
-﻿
-using System;
+﻿using System;
 
 namespace Program
 {
-    class prog
+    abstract class Shape
     {
-        static void Main(String[] args)
+        public abstract double calculateArea();
+        public abstract double calculatePerimeter();
+    }
+
+    class Circle : Shape
+    {
+        static readonly double PI = 3.14;
+        public double Radius { get; set; }
+
+        public override double calculateArea()
         {
-            int number;
-             
-            for(int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("please enter a number:");
-                number = Int32.Parse(Console.ReadLine());
-                Console.WriteLine("please enter a number:");
-                number = Int32.Parse(Console.ReadLine());
-                if (number >i)
-            {
-                Console.WriteLine(number +" is greater " );
-            }else if (number <i)
-            {
-                Console.WriteLine(number + " is lesser  ");
-            }
-            else
-            {
-                    Console.WriteLine(number + "is equal  "  );
-                        }
-            }
-            Console.ReadKey();
+            return PI * Radius * Radius;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * PI * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public override double calculateArea()
+        {
+            return Length * Width;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+    class Lab3
+    {
+        static void Main()
+        {
+            Circle circle = new Circle();
+            Rectangle rect = new Rectangle();
+
+            circle.Radius = 2;
+            rect.Width = 3;
+            rect.Length = 5;
+
+            Console.WriteLine("Radius: " + circle.Radius + "\nArea: " + circle.calculateArea());
+            Console.WriteLine("\nLength; " + rect.Length + "\nWidth: " + rect.Width + "\n" +
+                              "Area: " + rect.calculateArea() + "\nPerimeter: " + rect.calculatePerimeter());
 
         }
     }
